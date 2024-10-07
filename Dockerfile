ea3f28d6ee0a53c398d24ed4e19f5405e4201fb9
Nordin <vipnordin@gmail.com>
2024-10-08 06:56:05 +0800

    Refactor Dockerfile and compose.yaml for improved configuration and health checks

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,13 @@ FROM oven/bun:1.0.25
 
 LABEL maintainer="Nordin <vipnordin@gmail.com>"
 
-ENV APP_HOME=/code
+ENV APP_HOME=/app
 
 WORKDIR $APP_HOME
 
 COPY package.json $APP_HOME
 
-RUN bun install
+RUN ["bun", "install", "--no-cache"]
 
 COPY . $APP_HOME
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -20,29 +20,32 @@ export default async (request: Request, ctx: Context) => {
     const body = await request.json()
 
     switch (request.method) {
-    case 'GET':
-
-      response = new Response('GET method not supported', { status: 405 })
-      break
-    case 'POST':
-      try {
-        if (url.pathname === '/api/public/register') {
-          const result = await signUp(body, ctx)
-          response = new Response(JSON.stringify(result), { status: 200 })
-        } else if (url.pathname === '/api/public/login') {
-          const result = await signIn(body, ctx)
-          response = new Response(JSON.stringify(result), { status: 200 })
-        } else {
-          response = new Response('Not Found', { status: 404 })
+      case 'GET':
+        if (url.pathname === '/') {
+          response = new Response({ status: 'ok' }, { status: 200 })
+        } else if (url.pathname === '/health') {
+          response = new Response({ health: 'ok' }, { status: 200 })
         }
-      } catch (error: any) {
-        const errorMessage = error.message || 'Internal server error'
-        const statusCode = error.statusCode || 500
-        response = new Response(JSON.stringify({ error: errorMessage }), { status: statusCode })
-      }
-      break
-    default:
-      response = new Response('Method Not Allowed', { status: 405 })
+        break
+      case 'POST':
+        try {
+          if (url.pathname === '/api/public/register') {
+            const result = await signUp(body, ctx)
+            response = new Response(JSON.stringify(result), { status: 200 })
+          } else if (url.pathname === '/api/public/login') {
+            const result = await signIn(body, ctx)
+            response = new Response(JSON.stringify(result), { status: 200 })
+          } else {
+            response = new Response('Not Found', { status: 404 })
+          }
+        } catch (error: any) {
+          const errorMessage = error.message || 'Internal server error'
+          const statusCode = error.statusCode || 500
+          response = new Response(JSON.stringify({ error: errorMessage }), { status: statusCode })
+        }
+        break
+      default:
+        response = new Response('Method Not Allowed', { status: 405 })
     }
   } catch (error) {
     console.error('Error handling request:', error)
